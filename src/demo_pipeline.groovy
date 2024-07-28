8856be5d83fffd34dbb57bc36039451005233ce6
Ori Wizzz Wiesel <ori.w@uveye.com>
2024-07-28 11:51:15 +0300

    first upload

---
+++ src/demo_pipeline.groovy
@@ -0,0 +1,17 @@
+@Library(shared-lib) _
+
+pipeline{
+    agent{
+        node none
+    }
+
+    stages{
+        stage{
+            steps{
+                script{
+                    echo "hi"
+                }
+            }
+        }
+    }
+}
