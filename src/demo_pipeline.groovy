6d3c9539e0b0bb4fd4755a86bf2fd52c350e4300
Ori Wiesel <ori.w@uveye.com>
2024-08-02 16:37:13 +0300

    udpate

--- src/demo_pipeline.groovy
+++ src/demo_pipeline.groovy
@@ -2,8 +2,20 @@
 
 pipeline{
     agent any
+    // agent {
+    //     node {
+    //         label "master"
+    //     }
+    // }
 
     stages{
+        stage("checks"){
+            steps{
+                script{
+                    sh("ls -la")
+                }
+            }
+        }
         stage("git checkout"){
             steps{
                 script{
