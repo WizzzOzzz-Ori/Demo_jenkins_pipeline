a0c32a1aa4ad7fa86ddb5ecb2a48aad93fa5928f
Ori Wiesel <ori.w@uveye.com>
2024-08-02 22:54:31 +0300

    update

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,3 +1,9 @@
 def call(def tagName="${env.JOB_BASE_NAME.toLowerCase()}_${env.BUILD_NUMBER}"){
+    sh """
+    while (! docker info > /dev/null 2>&1); do
+        echo "Waiting for Docker daemon to start..."
+        sleep 1
+    done
+    """
     sh "docker build . -t ${tagName}"
 }
