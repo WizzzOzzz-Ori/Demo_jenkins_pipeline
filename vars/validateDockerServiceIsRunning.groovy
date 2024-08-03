be3c9618cb107e22328217e63a357665d53f5b78
Ori Wiesel <ori.w@uveye.com>
2024-08-03 22:17:24 +0300

    update

--- vars/validateDockerServiceIsRunning.groovy
+++ vars/validateDockerServiceIsRunning.groovy
@@ -2,7 +2,7 @@ def call(){
     sh """
     if ! docker info > /dev/null 2>&1;
     then
-        dockerd &
+        dockerd > /dev/null 2>&1 &
         while (! docker info > /dev/null 2>&1); do
             echo "Waiting for Docker daemon to start..."
             sleep 1
