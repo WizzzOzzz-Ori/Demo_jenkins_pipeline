03f4d27677f2adf31eaeed76a5a6a8f11a248f90
Ori Wiesel <ori.w@uveye.com>
2024-08-03 22:18:27 +0300

    update

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,4 +1,4 @@
-def call(def tag="${env.JOB_BASE_NAME}_${env.BUILD_NUMBER}"){
+def call(def tag="${env.JOB_BASE_NAME.toLowerCase()}_${env.BUILD_NUMBER}"){
     validateDockerServiceIsRunning()
     
     sh "docker push ${tag}"
