53885affe7be9aa98ed40dc84b1d367f019005de
Ori Wiesel <ori.w@uveye.com>
2024-08-03 21:37:51 +0300

    update

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,7 +2,7 @@ def call(){
     validateDockerServiceIsRunning()
     def tempTag = ""
     withDockerRegistry(credentialsId: 'dockerhub-credentials', url: 'https://index.docker.io/v1/') {
-        tempTag = "${DOCKERHUB_USERNAME}/${env.JOB_BASE_NAME}_${env.BUILD_NUMBER}"
+        tempTag = "${DOCKER_REGISTRY_USERNAME}/${env.JOB_BASE_NAME}_${env.BUILD_NUMBER}"
     }
     sh "docker build -t ${tempTag}."
     return tempTag
--- vars/dockerBuildAndTag.groovy
+++ vars/dockerBuildAndTag.groovy
@@ -2,7 +2,7 @@ def call(def tagName="${env.JOB_BASE_NAME.toLowerCase()}_${env.BUILD_NUMBER}"){
     validateDockerServiceIsRunning()
     def imageSha = dockerBuild()
     withDockerRegistry(credentialsId: 'dockerhub-credentials', url: 'https://index.docker.io/v1/') {
-        tagName = "${DOCKERHUB_USERNAME}/${tagName}"
+        tagName = "${DOCKER_REGISTRY_USERNAME}/${tagName}"
     }
     sh "docker image tag ${imageSha} ${tagName}"
 } 
