eb5b21b1273f100e860c24fbe591886d0bf42e01
Ori Wiesel <ori.w@uveye.com>
2024-08-03 22:08:03 +0300

    update

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -4,6 +4,6 @@ def call(){
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
         tempTag = "${DOCKERHUB_USERNAME}/${env.JOB_BASE_NAME}_${env.BUILD_NUMBER}"
     }
-    sh "docker build -t ${tempTag}."
+    sh "docker build -t ${tempTag} ."
     return tempTag
 }
