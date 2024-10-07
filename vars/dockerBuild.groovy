0354c92b9cb8a7562f1e35dd0ae3d7b0c8e5fb04
Ori Wiesel <ori.w@uveye.com>
2024-10-07 09:50:00 +0300

    add kubectl container

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,5 @@
 def call(){
-    // validateDockerServiceIsRunning()
+    validateDockerServiceIsRunning()
     def tempTag = ""
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
         tempTag = "${DOCKERHUB_USERNAME}/${env.JOB_BASE_NAME.toLowerCase()}:${env.BUILD_NUMBER}"
