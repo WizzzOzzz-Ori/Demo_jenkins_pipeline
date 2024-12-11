31eea1d182ad3bbfd810000f48aa72eb960b3411
ori wizzz wiesel <ori.w@uveye.com>
2024-12-11 11:05:27 +0200

    test

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,5 +1,5 @@
 def call(def tag="${env.JOB_BASE_NAME.toLowerCase()}_${env.BUILD_NUMBER}"){
-    // validateDockerServiceIsRunning()
+    validateDockerServiceIsRunning()
     
     sh "docker push ${tag}"
 }
