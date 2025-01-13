0f7d6f167cac205d05e6b96cdbf297eb2f5675a6
ori wizzz wiesel <ori.w@uveye.com>
2025-01-13 20:35:32 +0200

    tes

--- vars/envsubst.groovy
+++ vars/envsubst.groovy
@@ -1,5 +1,5 @@
 def call(String stringToModify, Map variablesToReplaceMap){
-    sh "apt update && apt-get install gettext-base -y"
+    sh "apk update && apk add gettext -y"
     def listOfEV = variablesToReplaceMap.collect { key, value -> "${key}=${value}" }
     withEnv(listOfEV){
         return sh(script: "echo ${stringToModify} | envsubst", returnStdout: true)
